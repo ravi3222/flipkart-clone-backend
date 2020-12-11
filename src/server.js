7cd631db616da1fcc13393ea8c9b174bb7812beb
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-12 00:07:37 +0530

    mongodb database connection

--- src/server.js
+++ src/server.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const env = require("dotenv");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 env.config();
+
+// mongodb
+// mongodb+srv://admin:<password>@cluster0.fd3mr.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.fd3mr.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Hello from server",
+  });
+});
+
+app.post("/data", (req, res, next) => {
+  res.status(200).json({
+    message: req.body,
   });
 });
 
