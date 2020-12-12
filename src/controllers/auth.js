f92bb8708d09d6b0bb841b4375987e04330b7149
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-12 21:14:54 +0530

    api routes for profile / requireSignin controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -63,3 +63,10 @@ exports.signin = (req, res) => {
     }
   });
 };
+
+exports.requireSignin = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = user;
+  next();
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/auth");
+const { signup, signin, requireSignin } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/signin", signin);
 
 router.post("/signup", signup);
 
+router.post("/profile", requireSignin, (req, res) => {
+  res.status(200).json({
+    user: "profile",
+  });
+});
 module.exports = router;
