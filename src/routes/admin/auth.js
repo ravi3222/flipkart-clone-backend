f5a3865febc1ad28525c6406fa6a57798cc9f4fa
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-14 23:13:24 +0530

    sign signup validation

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { signin, signup } = require("../../controllers/admin/auth");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../../validators/auth");
 const router = express.Router();
 
-router.post("/admin/signin", signin);
+router.post("/admin/signin", validateSigninRequest, isRequestValidated, signin);
 
-router.post("/admin/signup", signup);
+router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
 
 module.exports = router;
