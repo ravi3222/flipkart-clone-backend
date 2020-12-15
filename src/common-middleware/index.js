cc2c5e781efb5ff904fd60e165c5bc5357102fdf
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-15 22:51:22 +0530

    Admin middleware (For only admin access) to create category / sub-category

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -1,6 +1,26 @@
+const jwt = require("jsonwebtoken");
+
 exports.requireSignin = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
   const user = jwt.verify(token, process.env.JWT_SECRET);
   req.user = user;
+  next();
+};
+
+exports.userMiddleware = (req, res, next) => {
+  if (req.user.role !== "user") {
+    return res
+      .status(400)
+      .json({ message: "Access denied, You need user access" });
+  }
+  next();
+};
+
+exports.adminMiddleware = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(400).json({
+      message: "Access denied, You need admin access to add categories",
+    });
+  }
   next();
 };
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { requireSignin, adminMiddleware } = require("../common-middleware");
 const { addCategory, getCategories } = require("../controllers/category");
 const router = express.Router();
 
-router.post("/category/create", addCategory);
+router.post("/category/create", requireSignin, adminMiddleware, addCategory);
 router.get("/category/getcategory", getCategories);
 
 module.exports = router;
