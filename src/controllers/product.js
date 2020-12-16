fafb2f8dbb95db886a58813ea2af6768c22fc027
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-16 18:05:01 +0530

    File upload using multer in create product api

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 
 exports.createProduct = (req, res) => {
-  res.status(200).json({ message: "hello" });
+  // res.status(200).json({ message: "hello" });
+  res.status(200).json({ file: req.files, body: req.body });
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const { requireSignin, adminMiddleware } = require("../common-middleware");
 const { createProduct } = require("../controllers/product");
+const multer = require("multer");
 const router = express.Router();
+const shortid = require("shortid");
+const path = require("path");
 
-router.post("/product/create", requireSignin, adminMiddleware, createProduct);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post(
+  "/product/create",
+  requireSignin,
+  adminMiddleware,
+  upload.array("productImage"),
+  createProduct
+);
 
 module.exports = router;
