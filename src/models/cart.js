b0ecc9ac423a62ec6f90a89a460d636af4ec9aa3
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-18 14:36:55 +0530

    Update cart schema

--- src/models/cart.js
+++ src/models/cart.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     cartItems: [
       {
         product: {
