ba72217c1085fc559032a7008b531dfd12654bca
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-19 14:07:59 +0530

    Multer setup for File upload of category image / category routes

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,8 +2,28 @@ const express = require("express");
 const { requireSignin, adminMiddleware } = require("../common-middleware");
 const { addCategory, getCategories } = require("../controllers/category");
 const router = express.Router();
+const multer = require("multer");
+const shortid = require("shortid");
+const path = require("path");
 
-router.post("/category/create", requireSignin, adminMiddleware, addCategory);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post(
+  "/category/create",
+  requireSignin,
+  adminMiddleware,
+  upload.single("categoryImage"),
+  addCategory
+);
 router.get("/category/getcategory", getCategories);
 
 module.exports = router;
