c1466aa4dcb18beb55720d3eef650127981c499f
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-19 09:47:47 +0530

    fix: current item update override whole cart > updating respective items / cleanup

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -7,43 +7,36 @@ exports.addItemToCart = (req, res) => {
     if (cart) {
       const product = req.body.cartItems.product;
       const productInCart = cart.cartItems.find((c) => c.product == product);
-
+      let condition, update;
       if (productInCart) {
         console.log("Updating Cart by quantity");
-        Cart.findOneAndUpdate(
-          { user: req.user._id, "cartItems.product": product },
-          {
-            $set: {
-              cartItems: {
-                ...req.body.cartItems,
-                quantity: productInCart.quantity + req.body.cartItems.quantity,
-              },
+
+        condition = { user: req.user._id, "cartItems.product": product };
+        update = {
+          $set: {
+            "cartItems.$": {
+              ...req.body.cartItems,
+              quantity: productInCart.quantity + req.body.cartItems.quantity,
             },
-          }
-        ).exec((error, _cart) => {
-          if (error) return res.status(400).json({ error });
-          if (_cart) {
-            return res.status(201).json({ cart: _cart });
-          }
-        });
+          },
+        };
       } else {
         console.log("Updating Cart");
-        Cart.findOneAndUpdate(
-          { user: req.user._id },
-          {
-            $push: {
-              cartItems: req.body.cartItems,
-            },
-          }
-        ).exec((error, _cart) => {
-          if (error) return res.status(400).json({ error });
-          if (_cart) {
-            return res.status(201).json({ cart: _cart });
-          }
-        });
+
+        condition = { user: req.user._id };
+        update = {
+          $push: {
+            cartItems: req.body.cartItems,
+          },
+        };
       }
 
-      // return res.status(200).json({ message: "cart Exists" });
+      Cart.findOneAndUpdate(condition, update).exec((error, _cart) => {
+        if (error) return res.status(400).json({ error });
+        if (_cart) {
+          return res.status(201).json({ cart: _cart });
+        }
+      });
     } else {
       const cart = new Cart({
         user: req.user._id,
