ad963ad5c66ffd2d95f27d16486cb62c52f977e0
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-19 14:16:00 +0530

    Storing category image as url / api

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -26,6 +26,11 @@ exports.addCategory = (req, res) => {
     slug: slugify(req.body.name),
   };
 
+  if (req.file) {
+    categoryObj.categoryImage =
+      process.env.API + "/public/" + req.file.filename;
+  }
+
   if (req.body.parentId) {
     categoryObj.parentId = req.body.parentId;
   }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const env = require("dotenv");
 const app = express();
-// const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin/auth");
@@ -28,6 +28,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
