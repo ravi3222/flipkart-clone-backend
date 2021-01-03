788316b73f437050f0136521ca0cbe812cb86062
ravi3222 <kashyap.ravi3222@gmail.com>
2021-01-04 00:17:32 +0530

    refactor: asynchronuous password hashing using bcrypt for both admin and user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,20 +1,22 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.signup = (req, res) => {
-  User.findOne({ email: req.body.email }).exec((error, user) => {
+  User.findOne({ email: req.body.email }).exec(async (error, user) => {
     if (user)
       return res.status(400).json({
         message: "User already registered",
       });
 
     const { firstName, lastName, email, password } = req.body;
+    const hash_password = await bcrypt.hash(password, 10);
 
     const _user = new User({
       firstName,
       lastName,
       email,
-      password,
+      hash_password,
       username: Math.random().toString(),
     });
 
