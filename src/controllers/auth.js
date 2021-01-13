b5a3e630329eaaa12b5cccd4d1a82b1d2ab52a28
ravi3222 <kashyap.ravi3222@gmail.com>
2021-01-13 06:01:18 +0530

    Generated username in admin and user auth using shortid

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const shortid = require("shortid");
 
 exports.signup = (req, res) => {
   User.findOne({ email: req.body.email }).exec(async (error, user) => {
@@ -8,7 +9,6 @@ exports.signup = (req, res) => {
       return res.status(400).json({
         message: "User already registered",
       });
-
     const { firstName, lastName, email, password } = req.body;
     const hash_password = await bcrypt.hash(password, 10);
 
@@ -17,7 +17,7 @@ exports.signup = (req, res) => {
       lastName,
       email,
       hash_password,
-      username: Math.random().toString(),
+      username: shortid.generate(),
     });
 
     _user.save((error, data) => {
