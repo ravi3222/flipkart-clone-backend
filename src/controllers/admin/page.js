82b3ff2ce970d99a942bcbff8128b42b0d9a9d7c
ravi3222 <kashyap.ravi3222@gmail.com>
2021-04-14 02:22:13 +0530

    Add role in auth controller

--- src/controllers/admin/page.js
+++ src/controllers/admin/page.js
@@ -3,13 +3,13 @@ const Page = require("../../models/page");
 exports.createPage = (req, res) => {
   console.log("req", req.body, "req", req.files);
   const { banners, products } = req.files;
-  if (banners.length > 0) {
+  if (banners && banners.length > 0) {
     req.body.banners = banners.map((banner, index) => ({
       img: `${process.env.API}/public/${banner.filename}`,
       navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`,
     }));
   }
-  if (products.length > 0) {
+  if (products && products.length > 0) {
     req.body.products = products.map((product, index) => ({
       img: `${process.env.API}/public/${product.filename}`,
       navigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`,
@@ -47,10 +47,11 @@ exports.createPage = (req, res) => {
 
 exports.getPage = (req, res) => {
   const { category, type } = req.params;
+  console.log("getParams req.params", req.params);
   if (type === "page") {
     Page.findOne({ category: category }).exec((error, page) => {
       if (error) return res.status(400).json({ error });
-      if (page) return res.status(201).json({ page });
+      if (page) return res.status(200).json({ page });
     });
   }
 };
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -42,7 +42,7 @@ exports.signin = (req, res) => {
     if (error) return res.status(400).json({ error });
 
     if (user) {
-      if (user.authenticate(req.body.password)) {
+      if (user.authenticate(req.body.password) && user.role === "user") {
         const token = jwt.sign(
           { _id: user.id, role: user.role },
           process.env.JWT_SECRET,
